5d708adddcbdf9686accaef06147bbaf2dacfe44
KaranjaRic <karanja.erick04@gmail.com>
2023-10-21 17:15:06 -0700

    monty language

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_nop - Nothing
+ * @head: head of the stack
+ * @counter: line_number
+ * Return: Nothing
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
